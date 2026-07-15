29acb5236569b9515d5c2ece0615d4d15ac62ad0
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-07-15 14:31:22 +0530

    update

--- src/main/java/com/hbn/learning/Main.java
+++ src/main/java/com/hbn/learning/Main.java
@@ -10,21 +10,17 @@ import com.hbn.learning.entity.Employee;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		Employee emp = new Employee(1, "Ansh", "male", 99000);
-		
-		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-		
-		
-		SessionFactory sessionFactory = cfg.buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		Transaction transaction = session.beginTransaction();
-		
-		
+
+		Employee emp = new Employee(2, "ansh", "male", 99000);
+
+		Configuration cfg = new Configuration().configure("hibernate.cfgg.xml");// 1
+		SessionFactory sessionFactory = cfg.buildSessionFactory();// 2
+		Session session = sessionFactory.openSession();// 3
+		Transaction transaction = session.beginTransaction();// 4
+
 		session.persist(emp);
-		
 		transaction.commit();
+
 	}
 
 }
