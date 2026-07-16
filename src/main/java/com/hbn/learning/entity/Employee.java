2b5af2f533c2635bc71647808a2359701cde1ee6
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-07-16 13:27:54 +0530

    update

--- src/main/java/com/hbn/learning/entity/Employee.java
+++ src/main/java/com/hbn/learning/entity/Employee.java
@@ -1,9 +1,15 @@
 package com.hbn.learning.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-
+@Entity(name = "emppp")
 public class Employee {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name,gender;
 	private int salary;
