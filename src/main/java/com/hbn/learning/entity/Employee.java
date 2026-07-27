0e4d1f26fde5a783dd743c1ea1ad531a979c33e9
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-07-27 13:07:29 +0530

    update

--- src/main/java/com/hbn/learning/entity/Employee.java
+++ src/main/java/com/hbn/learning/entity/Employee.java
@@ -4,70 +4,64 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 
 @Entity(name = "emppp")
 public class Employee {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String name,gender;
+	private String name, gender;
 	private int salary;
-	
-	
+	@Transient
+	private String compName;
+
 	public Employee() {
 	}
-	
-	public Employee(String name, String gender, int salary) {
+
+	public Employee(String name, String gender, int salary,String compName) {
 		this.name = name;
 		this.gender = gender;
 		this.salary = salary;
+		this.compName = compName;
 	}
-
 
 	public int getId() {
 		return id;
 	}
 
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public String getGender() {
 		return gender;
 	}
 
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 
 	public int getSalary() {
 		return salary;
 	}
 
-
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
 	}
-	
+
 }
